f599bfadc3f8b36f77b65f8212f4b0b19bef8023
Celestia <308743744+sudocelestia@users.noreply.github.com>
2026-07-31 08:10:34 +1000

    test(verification): align module fixture

--- .github/scripts/verification_test.sh
+++ .github/scripts/verification_test.sh
@@ -525,13 +525,11 @@ go 1.26.5
 
 require (
 	github.com/BurntSushi/toml v1.6.0
+	golang.org/x/mod v0.38.0
 	golang.org/x/tools v0.48.0
 )
 
-require (
-	golang.org/x/mod v0.38.0 // indirect
-	golang.org/x/sync v0.22.0 // indirect
-)
+require golang.org/x/sync v0.22.0 // indirect
 EOF
   awk '
     $1 == "github.com/BurntSushi/toml" &&
